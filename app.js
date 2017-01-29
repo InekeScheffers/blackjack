3faf2123ed812fc87706b31747de1e913f3d883e
Ineke Scheffers <ineke@Jeroens-MacBook-Pro.local>
2017-01-29 18:17:29 +0100

    added comments what's next after installing new laptop

--- app.js
+++ app.js
@@ -6,25 +6,33 @@ const deck = require(__dirname + '/modules/deck');
 
 app.get('/start', (request, response) => {
 	console.log("About to render start...");
+
+//store deck, hand dealer, hand player, score dealer, score player in session
+
 	//deck.stackDeck();
 	let newShuffledDeck = deck.stackDeck();
 	console.log(newShuffledDeck);
+	// dealer gets first two cards of deck in his array/hand
 	let cardsDealer = [newShuffledDeck[0], newShuffledDeck[1]];
+	// delete these first two cards from deck
 	newShuffledDeck.shift();
 	newShuffledDeck.shift();
+	// user gets the next first two cards of deck in his array/hand
 	let cardsPlayer = [newShuffledDeck[0], newShuffledDeck[1]];
+	// delete these cards from deck
 	newShuffledDeck.shift();
 	newShuffledDeck.shift();
-	//var shuffledDeck = deck.stackDeck();
-	response.send([cardsDealer, cardsPlayer]);
-	//response.write("foo \nbar\n");
-	//...
-	//response.end()
+	// send first card of dealer + both cards of player
+	response.send([cardsDealer[0], cardsPlayer]);
 })
 
 app.get('/hit', (request, response) => {
 	console.log("About to render hit...");
-	response.send("Hit!");
+	//cardsPlayer.push(newShuffledDeck[0]);
+	//newShuffledDeck.shift();
+	//response.send([cardsDealer, cardsPlayer]);
+	response.send("Hit!")
+	// make game.pug render after /start, /hit, /stick and send data of all arrays to pug file with session?
 })
 
 app.get('/stick', (request, response) => {
