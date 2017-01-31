b33ce2caa6dcd2aab6a9c879319681e5e4c62f3d
Ineke Scheffers <ineke.scheffers@hotmail.com>
2017-01-31 20:03:58 +0100

    fixed error dealer.js

--- modules/dealer.js
+++ modules/dealer.js
@@ -5,35 +5,18 @@ const playDealer = (cardsDealer, cardDeck) => {
 
   let currentScoreDealer = score.getScore(cardsDealer);
   console.log(cardsDealer)
-  console.log(cardDeck)
   console.log(currentScoreDealer)
 
   // Check if dealer's total is < 17 then deal another card
   if (currentScoreDealer < 17) {
-    dealerGetCard(cardsDealer, cardDeck);
-    return;
-  }
-
-  // Check if the dealer busted/loses
-  if (currentScoreDealer > 21) {
-    return {
-      score: currentScoreDealer,
-      hand: cardsDealer
-    };
+    cardsDealer.push(deck.takeOneCard(cardDeck));
+    return playDealer(cardsDealer, cardDeck);
   }
 
   return {
     score: currentScoreDealer,
     hand: cardsDealer
   };
-}
-
-// if dealer has less than 17 and under 21, has to get another card and play again
-const dealerGetCard = (cardsDealer, cardDeck) => {
-  cardsDealer.push(deck.takeOneCard(cardDeck));
-  console.log(cardsDealer)
-  console.log(cardDeck)
-  playDealer(cardsDealer, cardDeck);
 }
 
 module.exports = {playDealer};
