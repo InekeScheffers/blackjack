66398dc25068374b230fa5a0c2a1a1ee153fba3b
Ineke Scheffers <ineke.scheffers@hotmail.com>
2017-02-01 15:59:49 +0100

    added backend check if /finish is requested when game is already finished rerender page with error message and offer to deal for a new game

--- routes/finish.js
+++ routes/finish.js
@@ -13,83 +13,93 @@ router.route('/finish')
   	console.log("About to finish...");
   	//response.send("Stick!");
   	let session = request.session;
-    // store gamestate in session (backend-check)
-		session.isFinished = true;
-    // generate current viewData so you can add results in the next steps
-    const finishViewData = viewData.generate(session);
-    // store gamestate in viewData (frontend-check)
-    finishViewData.isFinished = true;
+    // if the game is over and hit is still requested show error message and let player deal again
+		if(session.isFinished = undefined || session.isFinished){
+			let hitViewData = viewData.generate(session);
+			hitViewData.isFinished = true;
+			hitViewData.error = "Game is finished, first start a new game."
+			response.render('game', hitViewData)
+		}
+		// else finish!
+		else {
+      // store gamestate in session (backend-check)
+  		session.isFinished = true;
+      // generate current viewData so you can add results in the next steps
+      const finishViewData = viewData.generate(session);
+      // store gamestate in viewData (frontend-check)
+      finishViewData.isFinished = true;
 
-    // if player's score is over 21, you're always busted, show all dealer's cards
-    if(session.scorePlayer > 21){
-      // add total score and both cards to viewData
-      let currentScoreDealer = score.getScore(session.cardsDealer);
-      finishViewData.handDealer = session.cardsDealer;
-      finishViewData.scoreDealer = currentScoreDealer;
-
-      if(currentScoreDealer === 21 && session.cardsDealer.length < 3) {
-        finishViewData.result = "Busted, you lose! Dealer has blackjack!";
-      } else {
-      finishViewData.result = "Busted, you lose!";
-      }
-
-    }
-    // Player's score === 21 and this happened with the first two cards (so no extra hit)
-    else if(session.scorePlayer === 21 && session.cardsPlayer.length < 3) {
-
-      // check dealer's hand
-      let resultDealer = dealer.playDealer(session.cardsDealer, session.cardDeck);
-
-      finishViewData.handDealer = resultDealer.hand;
-      finishViewData.scoreDealer = resultDealer.score;
-
-      // if it's also 21, you both have blackjack and it's a push
-      if(resultDealer.score === 21) {
-        finishViewData.result = "Push";
-      } else {
-        finishViewData.result = "Blackjack! You win!";
-      }
-
-    }
-    // Player's score < 21
-    else {
-
-      // check dealer's hand first
-      let currentScoreDealer = score.getScore(session.cardsDealer);
-      // if it's 21 with his first two cards it's blackjack for the dealer
-      if(currentScoreDealer === 21 && session.cardsDealer.length < 3) {
+      // if player's score is over 21, you're always busted, show all dealer's cards
+      if(session.scorePlayer > 21){
+        // add total score and both cards to viewData
+        let currentScoreDealer = score.getScore(session.cardsDealer);
         finishViewData.handDealer = session.cardsDealer;
         finishViewData.scoreDealer = currentScoreDealer;
-        finishViewData.result = "Dealer has blackjack! You lose!";
+
+        if(currentScoreDealer === 21 && session.cardsDealer.length < 3) {
+          finishViewData.result = "Busted, you lose! Dealer has blackjack!";
+        } else {
+        finishViewData.result = "Busted, you lose!";
+        }
 
       }
-      // if dealer's score < 21
-      else {
-        // dealer takes cards > 17
+      // Player's score === 21 and this happened with the first two cards (so no extra hit)
+      else if(session.scorePlayer === 21 && session.cardsPlayer.length < 3) {
+
+        // check dealer's hand
         let resultDealer = dealer.playDealer(session.cardsDealer, session.cardDeck);
 
         finishViewData.handDealer = resultDealer.hand;
         finishViewData.scoreDealer = resultDealer.score;
 
-        // dealer's score is higher than player's and it's under or equal to 21
-        if(resultDealer.score > session.scorePlayer && resultDealer.score <= 21){
-          finishViewData.result = "Dealer wins! You lose!";
+        // if it's also 21, you both have blackjack and it's a push
+        if(resultDealer.score === 21) {
+          finishViewData.result = "Push";
+        } else {
+          finishViewData.result = "Blackjack! You win!";
         }
-        // dealer's score > 21
-        else if(resultDealer.score > 21){
-          finishViewData.result = "Dealer busted! You win!";
+
+      }
+      // Player's score < 21
+      else {
+
+        // check dealer's hand first
+        let currentScoreDealer = score.getScore(session.cardsDealer);
+        // if it's 21 with his first two cards it's blackjack for the dealer
+        if(currentScoreDealer === 21 && session.cardsDealer.length < 3) {
+          finishViewData.handDealer = session.cardsDealer;
+          finishViewData.scoreDealer = currentScoreDealer;
+          finishViewData.result = "Dealer has blackjack! You lose!";
+
         }
-        // it's a tie
-        else if(resultDealer.score === session.scorePlayer) {
-          finishViewData.result = "Push!";
-        }
-        // dealer > 17 but < 21, and < player's score
+        // if dealer's score < 21
         else {
-          finishViewData.result = "You win!";
+          // dealer takes cards > 17
+          let resultDealer = dealer.playDealer(session.cardsDealer, session.cardDeck);
+
+          finishViewData.handDealer = resultDealer.hand;
+          finishViewData.scoreDealer = resultDealer.score;
+
+          // dealer's score is higher than player's and it's under or equal to 21
+          if(resultDealer.score > session.scorePlayer && resultDealer.score <= 21){
+            finishViewData.result = "Dealer wins! You lose!";
+          }
+          // dealer's score > 21
+          else if(resultDealer.score > 21){
+            finishViewData.result = "Dealer busted! You win!";
+          }
+          // it's a tie
+          else if(resultDealer.score === session.scorePlayer) {
+            finishViewData.result = "Push!";
+          }
+          // dealer > 17 but < 21, and < player's score
+          else {
+            finishViewData.result = "You win!";
+          }
         }
       }
+      response.render('game', finishViewData);
     }
-    response.render('game', finishViewData);
   });
 
 //export this router
