1382b60382f55735b5da2ccd4b2fc0c1d78b0f11
Ineke Scheffers <ineke.scheffers@hotmail.com>
2017-02-01 15:57:30 +0100

    added backend check if hit is requested when game is finished, rerender page with error message and offer to deal for a new game

--- routes/hit.js
+++ routes/hit.js
@@ -10,21 +10,33 @@ const router = express.Router();
 
 router.route('/hit')
 	.get((request, response) => {
-  	console.log("Hit!");
-  	let session = request.session;
+		let session = request.session;
 
-  	// add a card to player's hand
-  	session.cardsPlayer.push(deck.takeOneCard(session.cardDeck));
-  	// calculate current score of player
-  	session.scorePlayer = score.getScore(session.cardsPlayer);
+		// if the game is over and hit is still requested show error message and let player deal again
+		if(session.isFinished = undefined || session.isFinished){
+			let hitViewData = viewData.generate(session);
+			hitViewData.isFinished = true;
+			hitViewData.error = "Game is finished, first start a new game."
+			response.render('game', hitViewData)
+		}
+		// else hit!
+		else {
+	  	console.log("Hit!");
+	  	// let session = request.session;
 
-  	// if it goes over or === 21 redirect to route /finish
-  	if(session.scorePlayer >= 21) {
-  		return response.redirect('finish');
-  	}
+	  	// add a card to player's hand
+	  	session.cardsPlayer.push(deck.takeOneCard(session.cardDeck));
+	  	// calculate current score of player
+	  	session.scorePlayer = score.getScore(session.cardsPlayer);
 
-  	// if it is under 21 render the game with the newly drawn card and score so player can choose hit or stick again
-  	response.render('game', viewData.generate(session));
+	  	// if it goes over or === 21 redirect to route /finish
+	  	if(session.scorePlayer >= 21) {
+	  		return response.redirect('finish');
+	  	}
+
+	  	// if it is under 21 render the game with the newly drawn card and score so player can choose hit or stick again
+	  	response.render('game', viewData.generate(session));
+		}
   })
 
 //export this router
